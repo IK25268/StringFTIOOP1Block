03f463e322c4698213dc3fb6094afdd8bac09ba7
IK25268 <162690760+IK25268@users.noreply.github.com>
2024-04-27 21:08:43 +0700

    String1.2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include "String.hpp"
-using namespace std;
 
 int main()
 {
-	String test1("qwertyuiop");
-	String test2("QWERTYUIOP");
-	String test3(test2);
-	test3 = test1;
 	return 0;
 }
